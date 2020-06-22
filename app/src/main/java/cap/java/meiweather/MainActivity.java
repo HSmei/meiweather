cdaae050e96062f44eca727a1bd2c453a3113aea
HSmei <448169503@qq.com>
2020-06-22 16:07:00 +0800

    First commit.

---
+++ app/src/main/java/cap/java/meiweather/MainActivity.java
@@ -0,0 +1,14 @@
+package cap.java.meiweather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
